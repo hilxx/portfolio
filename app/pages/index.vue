2ea120dcdca0482ef67d1c079957b29f85417dc8
hilxx <eunjung377@naver.com>
2026-03-25 22:02:14 +0900

    [수정] 페이지마다 해당 scss 파일만 연결

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -88,3 +88,8 @@ const projects: Project[] = [
   }
 ]
 </script>
+
+<style lang="scss" scoped>
+@use "@/assets/scss/pages/main.scss"
+</style>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['@/assets/scss/main.scss'],
+  css: ['@/assets/scss/index.scss'],
   vite: {
     css: {
       preprocessorOptions: {
